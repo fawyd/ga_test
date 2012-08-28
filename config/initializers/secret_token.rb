ec1f0e21652710e418316c1fd1a3ac1c366465a7
eicca <wtltl2@gmail.com>
2012-08-28 09:33:12 +0400

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GaTest::Application.config.secret_token = '8ec3943962e1b992cf266373281f8f1fca3fad8d9201df25b1404c59195d4d19999f230d0c2999e2b6cf9712cfc35d6c171dc9b9a56095010c1982263c90a52f'
